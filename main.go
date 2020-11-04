b9689ba62f8d6d16a687bde27f19b4c28b403079
Huan Jiang <bill.jiangh@gmail.com>
2020-11-05 01:52:40 +0800

    minor changes in comments

--- main.go
+++ main.go
@@ -8,14 +8,13 @@ import (
 var allStations []Station
 
 func main() {
-	// open the csv file
+	// load all stations from csv file
 	csvFile, err := os.Open("./data/StationMap.csv")
 	if err != nil {
 		panic(err)
 	}
 	defer csvFile.Close()
 
-	// read all stations
 	allStations, err = ReadStations(csvFile)
 	if err != nil {
 		panic(err)
