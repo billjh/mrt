e5bfaad98fbadf84929a2242a91d0b6b4caee2a7
Huan Jiang <bill.jiangh@gmail.com>
2020-11-06 22:49:08 +0800

    merge reader.go into station.go

--- station.go
+++ station.go
@@ -1,22 +1,24 @@
 package main
 
 import (
+	"encoding/csv"
 	"fmt"
+	"io"
 	"regexp"
 	"strconv"
 	"strings"
 	"time"
 )
 
-// StationCode is the code for a MRT station in Singapore
-// It consists of two fields, 2-alphabet MRT line and int station number
+// StationCode is the code for a MRT station in Singapore.
+// It consists of two fields, 2-alphabet MRT line and int station number.
 // For example, StationCode{line: "EW", number: 14}
 type StationCode struct {
 	line   string
 	number int
 }
 
-// Station represents a MRT station in Singapore
+// Station represents a MRT station in Singapore.
 type Station struct {
 	code        StationCode
 	name        string
@@ -24,7 +26,7 @@ type Station struct {
 }
 
 // NewStationCode constructs a StationCode from string
-// and returns error on invalid format
+// and returns error on invalid format.
 func NewStationCode(code string) (StationCode, error) {
 	matched, err := regexp.MatchString(`^[a-zA-Z]{2}\d{1,2}$`, code)
 	if err != nil {
@@ -39,4 +41,55 @@ func NewStationCode(code string) (StationCode, error) {
 		return StationCode{}, err
 	}
 	return StationCode{line: strings.ToUpper(code[:2]), number: number}, nil
+}
+
+// ReadStations reads the stations from the given io.Reader.
+// It assumes the format being:
+//
+// Station Code,Station Name,Opening Date
+// EW23,Clementi,12 March 1988
+// EW24,Jurong East,5 November 1988
+// EW25,Chinese Garden,5 November 1988
+// EW26,Lakeside,5 November 1988
+//
+func ReadStations(r io.Reader) ([]Station, error) {
+	csvReader := csv.NewReader(r)
+
+	// skip header row
+	_, err := csvReader.Read()
+	if err != nil {
+		return nil, err
+	}
+
+	records, err := csvReader.ReadAll()
+	if err != nil {
+		return nil, err
+	}
+
+	// parse each row and store in the final result array
+	final := []Station{}
+
+	// the format used by time.Parse function
+	const openingDateFormat string = "2 January 2006"
+
+	for _, record := range records {
+		if len(record) != 3 {
+			return nil, fmt.Errorf("record lenth not 3: %v", record)
+		}
+		code, err := NewStationCode(record[0])
+		if err != nil {
+			return nil, err
+		}
+		openingDate, err := time.Parse(openingDateFormat, record[2])
+		if err != nil {
+			return nil, err
+		}
+		final = append(final, Station{
+			code:        code,
+			name:        record[1],
+			openingDate: openingDate,
+		})
+	}
+
+	return final, nil
 }
--- station_test.go
+++ station_test.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"strings"
 	"testing"
+	"time"
 )
 
 func TestNewStationCodeSuccess(t *testing.T) {
@@ -45,6 +47,56 @@ func TestNewStationCodeError(t *testing.T) {
 		_, err := NewStationCode(testCase)
 		if err == nil {
 			t.Errorf("expect error for input: %s", testCase)
+		}
+	}
+}
+
+func TestReadStations(t *testing.T) {
+	for _, testCase := range []struct {
+		fileContent string
+		expected    []Station
+	}{
+		{
+			fileContent: "Station Code,Station Name,Opening Date\n" +
+				"EW23,Clementi,12 March 1988\n" +
+				"EW24,Jurong East,5 November 1988\n" +
+				"EW25,Chinese Garden,5 November 1988\n" +
+				"EW26,Lakeside,5 November 1988\n",
+			expected: []Station{
+				Station{
+					code:        StationCode{line: "EW", number: 23},
+					name:        "Clementi",
+					openingDate: time.Date(1988, 3, 12, 0, 0, 0, 0, time.UTC),
+				},
+				Station{
+					code:        StationCode{line: "EW", number: 24},
+					name:        "Jurong East",
+					openingDate: time.Date(1988, 11, 5, 0, 0, 0, 0, time.UTC),
+				},
+				Station{
+					code:        StationCode{line: "EW", number: 25},
+					name:        "Chinese Garden",
+					openingDate: time.Date(1988, 11, 5, 0, 0, 0, 0, time.UTC),
+				},
+				Station{
+					code:        StationCode{line: "EW", number: 26},
+					name:        "Lakeside",
+					openingDate: time.Date(1988, 11, 5, 0, 0, 0, 0, time.UTC),
+				},
+			},
+		},
+	} {
+		s, err := ReadStations(strings.NewReader(testCase.fileContent))
+		if err != nil {
+			t.Error(err)
+		}
+		if len(s) != len(testCase.expected) {
+			t.Errorf("len not match, expected: %d, actual: %d", len(testCase.expected), len(s))
+		}
+		for i, actual := range s {
+			if actual != testCase.expected[i] {
+				t.Errorf("item not match, expected: %v, actual: %v", testCase.expected[i], actual)
+			}
 		}
 	}
 }
